620c97a3ab7ccdf47efc48beae75ed3e1caac6d9
belavigispradeep <pradeepsbelavigi26@gmail.com>
2024-03-13 00:49:56 +0530

    Add the button which convert lowercase to uppercase

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import "./App.css";
 import Navbar from "./components/Navbar";
+import TextFrom from "./components/TextFrom";
 
 function App() {
   return (
     <>
       <Navbar title = "TextUtils" aboutText = "About Us"/>
+      <div className="container my-3 "><TextFrom title = "Enter the text to analyze below"/></div>
     </>
   );
 }
--- src/components/TextFrom.js
+++ src/components/TextFrom.js
@@ -1,29 +1,33 @@
-import React from "react";
+import React,{useState} from "react";
 
-export default function TextFrom() {
+
+export default function TextFrom(props) {
+    const handleUpClick = () =>{
+        console.log("UpperCase was clicked" + text);
+        let newText = text.toUpperCase();
+        setText(newText);
+    };
+    const handleOnChange = (event) =>{
+        console.log("onchange called");
+        setText(event.target.value);
+    };
+    
+    const [text, setText] = useState("Enter text here");
   return (
     <div>
       <div className="mb-3">
-        <label for="exampleFormControlInput1" className="form-label">
-          Email address
-        </label>
-        <input
-          type="email"
-          className="form-control"
-          id="exampleFormControlInput1"
-          placeholder="name@example.com"
-        />
-      </div>
-      <div className="mb-3">
-        <label for="exampleFormControlTextarea1" className="form-label">
-          Example textarea
+        <label htmlFor="myBox" className="form-label">
+          <h3>{props.title}</h3>
         </label>
         <textarea
           className="form-control"
-          id="exampleFormControlTextarea1"
-          rows="3"
+          value={text}
+          onChange={handleOnChange}
+          id="myBox"
+          rows="10"
         ></textarea>
       </div>
+      <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
     </div>
   );
 }
