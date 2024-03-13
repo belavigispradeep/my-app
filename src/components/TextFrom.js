3d57ae3a0bcbf466aaa6c5f02e3057b68edb0a5a
belavigispradeep <pradeepsbelavigi26@gmail.com>
2024-03-13 10:10:16 +0530

    Add the Lowercase button and we count the number of world and character in sentance

--- src/components/TextFrom.js
+++ src/components/TextFrom.js
@@ -7,12 +7,17 @@ export default function TextFrom(props) {
         let newText = text.toUpperCase();
         setText(newText);
     };
+    const handleLoClick = () =>{
+      let  newText =  text.toLowerCase()
+      setText(newText);
+    }
     const handleOnChange = (event) =>{
         setText(event.target.value);
     };
 
     const [text, setText] = useState("Enter text here");
   return (
+    <>
     <div>
       <div className="mb-3">
         <label htmlFor="myBox" className="form-label">
@@ -27,6 +32,13 @@ export default function TextFrom(props) {
         ></textarea>
       </div>
       <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
+      <button className="btn btn-danger mx-4"  onClick={handleLoClick}>Convert to Lowercase</button>
     </div>
+    <div className="container my-3">
+      <h3>Your text summary</h3>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008*text.split(" ").length} minutes per words </p>
+    </div>
+    </>
   );
 }
